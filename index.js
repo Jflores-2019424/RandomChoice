a78e76fdfc5c2d5592ce1050610a50f39c4f4dfe
Jflores-2019424 <jflores-2019424@kinal.edu.gt>
2023-09-18 10:43:25 -0600

    Proyecto completo

--- index.js
+++ index.js
@@ -1,15 +1,64 @@
 const textarea = document.getElementById("textarea")
-const options = document.getElementById("options")
+const optionList = document.getElementById("options")
 
 function addOptions(input){
-    const options = input.split(',').filter(options => options.trim() !== '').map(options => options.trim())
+    const op = input.split(',').filter(options => options.trim() !== '').map(options => options.trim())
 
-    options.innerHTML = ''
+    optionList.innerHTML = ''
 
-    options.forEach(options => {
-        const options = document.createElement('span')
-        options.classList.add('options')
-        options.innerText = options
-        options.appendChild(options)
+    op.forEach(options => {
+        const option = document.createElement('span')
+        option.classList.add('options')
+        option.innerText = options
+        optionList.appendChild(option)
     })
+}
+
+textarea.addEventListener('keyup', (e)=>{
+    addOptions(e.target.value)
+
+    if(e.key === "Enter"){
+        setTimeout(() =>{
+            e.target.value = ''
+        }, 10)
+
+        randomOption()
+    }
+})
+
+function randomOption(){
+    const times = 25
+
+    const interval = setInterval(() =>{
+        const randomOp = pickOption()
+
+        selectedOption(randomOp)
+
+        setTimeout(() => {
+            unselectedOption(randomOp)
+        }, 100)
+    }, 100)
+
+    setTimeout(() =>{
+        clearInterval(interval)
+
+        setTimeout(() =>{
+            const randomOp = pickOption()
+
+            selectedOption(randomOp)    
+        }, 100)
+    }, times * 100)
+}
+
+function pickOption() {
+    const op = document.querySelectorAll('.options')
+    return op[Math.floor(Math.random() * op.length)]
+}
+
+function selectedOption(options){
+    options.classList.add('selected')
+}
+
+function unselectedOption(options){
+    options.classList.remove('selected')
 }
