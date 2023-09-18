47da11af98d5f5f8568992ceda8ab86db07dd50a
Jflores-2019424 <jflores-2019424@kinal.edu.gt>
2023-09-18 10:10:21 -0600

    Logica

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const textarea = document.getElementById("textarea")
 const options = document.getElementById("options")
+
+function addOptions(input){
+    const options = input.split(',').filter(options => options.trim() !== '').map(options => options.trim())
+
+    options.innerHTML = ''
+
+    options.forEach(options => {
+        const options = document.createElement('span')
+        options.classList.add('options')
+        options.innerText = options
+        options.appendChild(options)
+    })
+}
